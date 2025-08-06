36780920116efe6ce7e5c40b649120f4e922d485
FBI_GATO_J <jhancielsl@gmail.com>
2025-08-06 14:01:56 -0400

    Add files via upload

---
+++ fb-safezone/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+version '0.0.1'
+description 'Zonas (Segura, Roja, Carga) con HUD y vms_notify'----puedes cambiar el vms-notify por el que uses
+author 'fbigato_j'-----Porfavor no remover el author
+
+client_scripts {
+    'config.lua',
+    'client/*.lua',
+}
+
+server_scripts {
+    'server/version_check.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
