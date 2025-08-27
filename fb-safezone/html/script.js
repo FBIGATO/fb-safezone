59218b28ff433d725a1e54b6d0952d27427b2b60
FBI_GATO_J <jhancielsl@gmail.com>
2025-08-27 13:06:55 -0400

    Update script.js

--- fb-safezone/html/script.js
+++ fb-safezone/html/script.js
@@ -1,12 +1,19 @@
 window.addEventListener('message', function (event) {
-    const zona = document.getElementById('safe-zone');
-  
-    if (event.data.action === 'showZone') {
-      zona.innerText = event.data.label || "Zona";
+  const zona = document.getElementById('safe-zone');
+  const icono = document.getElementById('zone-icon');
+  const texto = document.getElementById('zone-text');
+
+  if (event.data.action === 'showZone') {
+      texto.innerText = event.data.label || "Zona";
       zona.style.backgroundColor = event.data.color || "gray";
+      
+      // Cambiar ícono si viene en el mensaje
+      if (event.data.icon) {
+          icono.className = event.data.icon;
+      }
+
       zona.classList.remove('hidden');
-    } else if (event.data.action === 'hideZone') {
+  } else if (event.data.action === 'hideZone') {
       zona.classList.add('hidden');
-    }
-  });
-  
+  }
+});
